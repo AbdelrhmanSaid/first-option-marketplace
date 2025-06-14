179a3394e90249bf7e64075f3a6a938d5d356170
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-06-15 02:04:44 +0300

    Remove unnecessary route parameter from page header in dashboard static pages view

--- resources/views/dashboard/static-pages/index.blade.php
+++ resources/views/dashboard/static-pages/index.blade.php
@@ -1,4 +1,4 @@
 <x-layouts::dashboard>
-    <x-page-header :create="route('dashboard.static-pages.create')" class="mb-3" />
+    <x-page-header class="mb-3" />
     <livewire:datatables.static-pages />
 </x-layouts::dashboard>
