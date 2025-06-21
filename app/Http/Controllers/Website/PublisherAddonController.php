6d29218df112a97e3da1d5eed4bb39f2c8e71606
AbdelrhmanSaid <70618755+AbdelrhmanSaid@users.noreply.github.com>
2025-06-21 17:39:25 +0000

    fix code style

--- app/Http/Controllers/Website/PublisherAddonController.php
+++ app/Http/Controllers/Website/PublisherAddonController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
-use App\Models\Addon;
 use Illuminate\Http\Request;
 
 class PublisherAddonController extends Controller
