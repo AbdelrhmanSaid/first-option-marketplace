653781d602f44525993611ce50bd1649bcb1d756
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-06-26 17:54:38 +0300

    Adjust layout of add-on show view for improved responsiveness by modifying column widths; update purchase button logic to handle free downloads when no price is set.

--- resources/views/website/addons/partials/purchase.blade.php
+++ resources/views/website/addons/partials/purchase.blade.php
@@ -1,5 +1,5 @@
 <div class="card mb-3">
-    <div class="card-body text-center">
+    <div class="card-body">
         <div class="mb-3">
             @if ($addon->price)
                 <div class="h2 text-success mb-1">
@@ -17,22 +17,18 @@
             @endif
         </div>
 
-        @if ($addon->price)
-            <button class="btn btn-primary w-100 mb-2">
-                <i class="fas fa-shopping-cart me-2"></i>
-                {{ __('Purchase Now') }}
-            </button>
-            @if ($addon->trial_period)
-                <button class="btn w-100 mb-3">
-                    <i class="fas fa-play me-2"></i>
-                    {{ __('Start Free Trial') }}
+        <div>
+            @if ($addon->price)
+                <button class="btn btn-primary w-100">
+                    <i class="fas fa-shopping-cart me-2"></i>
+                    {{ __('Purchase Now') }}
+                </button>
+            @else
+                <button class="btn btn-primary w-100">
+                    <i class="fas fa-download me-2"></i>
+                    {{ __('Download Free') }}
                 </button>
             @endif
-        @else
-            <button class="btn btn-success w-100 mb-3">
-                <i class="fas fa-download me-2"></i>
-                {{ __('Download Free') }}
-            </button>
-        @endif
+        </div>
     </div>
 </div>
--- resources/views/website/addons/show.blade.php
+++ resources/views/website/addons/show.blade.php
@@ -1,13 +1,13 @@
 <x-layouts::website :title="$addon->name">
     <div class="container py-4">
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-9">
                 @include('website.addons.partials.main')
                 @include('website.addons.partials.description')
                 @include('website.addons.partials.links')
             </div>
 
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <div class="sidebar-sticky">
                     @include('website.addons.partials.purchase')
                     @include('website.addons.partials.details')
