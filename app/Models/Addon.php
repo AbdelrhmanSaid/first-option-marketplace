a3485b98bfd57e0f026fc33ff7297804863c6618
AbdelrhmanSaid <70618755+AbdelrhmanSaid@users.noreply.github.com>
2025-07-07 15:25:36 +0000

    fix code style

--- app/Models/Addon.php
+++ app/Models/Addon.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
-use App\Models\AddonRate;
 
 class Addon extends Model
 {
